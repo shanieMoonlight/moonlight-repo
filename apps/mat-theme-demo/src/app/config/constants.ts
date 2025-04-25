c1fa716548e79f56cb7927e624511a90d789f4f2
shanebrannick <shanebrannick@yahoo.com>
2025-04-25 19:40:11 +0100

    spider-baby - home - sectioned

--- apps/mat-theme-demo/src/app/config/constants.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-export class AppConstants {
-
-    static readonly GIT_REPO = 'https://github.com/shanieSpiderBaby/spider-baby-repo'
-    static readonly NPM_PKG = 'https://www.npmjs.com/';
-
-}
