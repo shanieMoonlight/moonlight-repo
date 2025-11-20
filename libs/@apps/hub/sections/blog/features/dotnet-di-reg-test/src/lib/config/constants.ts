923a5561b1bc68d476448f148dda33f5f5041b42
shanebrannick <shanebrannick@yahoo.com>
2025-11-20 18:32:51 +0000

    blog/dotnet-di-reg-test - fixed the Description

--- libs/@apps/hub/sections/blog/features/dotnet-di-reg-test/src/lib/config/constants.ts
+++ libs/@apps/hub/sections/blog/features/dotnet-di-reg-test/src/lib/config/constants.ts
@@ -2,9 +2,8 @@ export const BlogConstants = {
   AuthServiceTutorial: {
     Title: '.Net Di Registration Testing',
     Subtitle: 'Ensure all your .Net services are correctly registered for Dependency Injection',
-    Description: `Learn how to implement a robust JWT authentication system in Angular using signals. 
-    This tutorial covers the BaseAuthSignalService architecture, JWT token handling, and how to extend 
-    the base service for your specific authentication needs.`,
+    Description: `Learn how to implement a bulletproof unit testing pattern for .NET Dependency Injection. 
+    This tutorial demonstrates how to use reflection to automatically verify that all your library's interfaces are registered, eliminating brittle test code and preventing runtime resolution errors.`,
     GitHubRepo: 'https://github.com/shanieMoonlight/spider-baby-identity/blob/master/Identity/Tests/Id.Tests.Utility/LibrarySetup/ServiceCollectionReflectionHelper.cs',
     // NpmPackage: 'https://www.npmjs.com/package/@spider-baby/auth-signal'
   }
